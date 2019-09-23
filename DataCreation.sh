58abb0a106a5ef5654e3dfb7e115f6b39023a364
Manish Raut <manish@infracloud.io>
2019-09-23 12:50:52 +0530

    modified the dbcraetion and added architecture diagram

--- DataCreation.sh
+++ DataCreation.sh
@@ -3,7 +3,7 @@ tserver="yb-v-tserver-0"
 files=("orders" "products" "reviews" "users")
 
 function getFiles() {
-    mkdir demo_data && cd $_
+    mkdir data && cd $_
     wget https://raw.githubusercontent.com/yugabyte/yb-sql-workshop/master/query-using-bi-tools/schema.sql
     wget https://github.com/yugabyte/yb-sql-workshop/raw/master/query-using-bi-tools/sample-data.tgz
     tar zxvf sample-data.tgz
@@ -11,8 +11,6 @@ function getFiles() {
     kubectl exec $tserver mkdir data
 }
 getFiles
-
-sleep 3s
 
 function copyFiles() {
    arr=("$@")
@@ -25,20 +23,16 @@ function copyFiles() {
 copyFiles "Copying" "${files[@]}"
 
 function createDatabase() {
-    //Run ysqlsh to connect to the service:-
-    kubectl exec -it $tserver /home/yugabyte/bin/ysqlsh -- -h $tserver  --echo-queries
-    sleep 10s
-    //Create a database.
+    kubectl exec -it $tserver /home/yugabyte/bin/ysqlsh -- -h $tserver  --echo-queries <<EOF
     CREATE DATABASE yb_demo;
     GRANT ALL ON DATABASE yb_demo to postgres;
     \c yb_demo;
-    //Insert sample data, First create the 4 tables necessary to store the data.
     \i 'schema.sql';
-    sleep 5s
-    //Now load the data into the tables.
     \i 'data/products.sql'
     \i 'data/users.sql'
     \i 'data/orders.sql'
     \i 'data/reviews.sql'
+EOF
 }
 createDatabase
+
